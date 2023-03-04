d76ca721bd1d355afe12dd4a33ee11d5d667f0da
YanHe <ahheyan@foxmail.com>
2023-03-04 21:28:06 +0800

    photo

---
+++ app/photo/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from extension.base.model import BaseModel
+from app.user.models import User
+from app.album.models import Album
+
+
+STATUS_CHOICES = (("0", "公开"), ("1", "私密"))
+
+
+class Photo(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="作者")
+    album = models.ForeignKey(
+        Album, on_delete=models.CASCADE, null=True, verbose_name="相册"
+    )
+    name = models.CharField(max_length=32, unique=True, verbose_name="照片名")
+    url = models.CharField(max_length=255, blank=True, default="", verbose_name="照片地址")
+    desc = models.CharField(max_length=255, blank=True, default="", verbose_name="描述")
+    status = models.CharField(
+        max_length=1, choices=STATUS_CHOICES, default="0", verbose_name="状态"
+    )
+
+    class Meta:
+        db_table = "photo"
+        verbose_name = "照片表"
+        verbose_name_plural = verbose_name
